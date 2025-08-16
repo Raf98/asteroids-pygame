3f1be951053fe6f955076d3b0ce5e7f02e37cf34
raf98 <rafaelrutzsantos@gmail.com>
2025-08-15 22:25:49 -0300

    Added start and game over screens; added a new running variable to control the main loop exit and show the game over screen.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
+from start_screen import StartScreen
+from game_over_screen import GameOverScreen
 
 def main():
     print("Starting Asteroids!")
@@ -17,6 +19,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    center_x = SCREEN_WIDTH / 2
+    center_y = SCREEN_HEIGHT / 2
+
+    start = StartScreen(screen, center_x, center_y)
+    start.draw_start_screen()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -28,15 +36,16 @@ def main():
     AsteroidField.containers = (updatable)
     Shot.containers = (shots, updatable, drawable)
 
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    
+    player = Player(center_x, center_y)
     asteroid_field = AsteroidField()
 
-    while True:
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+                break
 
         updatable.update(dt)
 
@@ -47,7 +56,9 @@ def main():
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 print("Game over!")
-                sys.exit()
+                running = False
+                break
+            
             for bullet in shots:
                 if asteroid.collides_with(bullet):
                     asteroid.split()
@@ -55,6 +66,10 @@ def main():
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
+
+    game_over = GameOverScreen(screen, center_x, center_y)
+    game_over.draw_game_over_screen()
 
 
 if __name__ == "__main__":
---
+++ start_screen.py
@@ -0,0 +1,48 @@
+import pygame
+import sys
+
+from constants import BLINK_INTERVAL
+
+class StartScreen():
+    def __init__(self, screen: pygame.Surface, center_x: int, center_y: int):
+        self.screen = screen
+        pygame.font.init()
+        self.title_font = pygame.font.SysFont(None, 96) 
+        self.prompt_font = pygame.font.SysFont(None, 48)
+        self.center_x = center_x
+        self.center_y = center_y
+
+
+    def draw_start_screen(self):
+        pygame.display.set_caption("Asteroids")
+
+        last_blink_time = pygame.time.get_ticks()
+        show_prompt_text = True
+        while True:
+            current_time = pygame.time.get_ticks()
+
+            if current_time - last_blink_time >= BLINK_INTERVAL:
+                show_prompt_text = not show_prompt_text
+                last_blink_time = current_time
+
+            self.draw_text(show_prompt_text)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    return
+
+
+    def draw_text(self, show_prompt_text: bool):
+        self.screen.fill("green")
+
+        title_text = self.title_font.render("Asteroids", True, "white")
+        self.screen.blit(title_text, title_text.get_rect(center=(self.center_x, self.center_y)))
+
+        if show_prompt_text:
+            prompt_text = self.prompt_font.render("Press any key to start", True, "white")
+            prompt_text_rect = prompt_text.get_rect(center=(self.center_x, self.center_y + 100))
+            self.screen.blit(prompt_text, prompt_text_rect)
+        pygame.display.flip()
